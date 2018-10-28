66c22a4f9ac4b887694153ea1e9f1c1536e58b6f
Wode <wode490390@qq.com>
2018-10-29 06:41:56 +0800

    todo

--- src/main/java/cn/nukkit/block/BlockIceFrosted.java
+++ src/main/java/cn/nukkit/block/BlockIceFrosted.java
@@ -2,9 +2,6 @@ package cn.nukkit.block;
 
 import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemBlock;
-import cn.nukkit.level.Level;
-import cn.nukkit.math.NukkitRandom;
-import cn.nukkit.utils.BlockColor;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,18 +22,15 @@ public class BlockIceFrosted extends BlockIce {
 
     @Override
     public boolean onBreak(Item item) {
-        this.getLevel().setBlock(this, new BlockWater(), true);
+        this.getLevel().setBlock(this, new BlockWaterStill(), true);
         List<Block> nearFrosted = new ArrayList<Block>();
-        int floorX = this.getFloorX();
-        int coordX1 = floorX - 1;
-        int coordX2 = floorX + 1;
-        int floorZ = this.getFloorZ();
-        int coordZ1 = floorZ - 1;
-        int coordZ2 = floorZ + 1;
-        int floorY = this.getFloorY();
+        int coordX1 = this.x - 1;
+        int coordX2 = this.x + 1;
+        int coordZ1 = this.z - 1;
+        int coordZ2 = this.z + 1;
         for (int coordX = coordX1; coordX < coordX2 + 1; coordX++) {
             for (int coordZ = coordZ1; coordZ < coordZ2 + 1; coordZ++) {
-                Block nearBlock = this.getLevel().getBlock(coordX, floorY, coordZ);
+                Block nearBlock = this.getLevel().getBlock(coordX, this.y, coordZ);
                 if (nearBlock instanceof BlockIceFrosted) {
                     nearFrosted.add(nearBlock);
                 }
@@ -45,59 +39,16 @@ public class BlockIceFrosted extends BlockIce {
         }
         if (nearFrosted.size() < 2) {
             for (Block iceBlock : nearFrosted) {
-                this.getLevel().setBlock(iceBlock, new BlockWater(), true);
+                this.getLevel().setBlock(iceBlock, new BlockWaterStill(), true);
             }
         }
         return true;
     }
 
-    @Override
-    public int onUpdate(int type) {
-        if (type == Level.BLOCK_UPDATE_RANDOM) {
-            if (this.getLevel().getBlockLightAt((int) this.x, (int) this.y, (int) this.z) > 11) {
-                List<Block> nearFrosted = new ArrayList<Block>();
-                int floorX = this.getFloorX();
-                int coordX1 = floorX - 1;
-                int coordX2 = floorX + 1;
-                int floorZ = this.getFloorZ();
-                int coordZ1 = floorZ - 1;
-                int coordZ2 = floorZ + 1;
-                int floorY = this.getFloorY();
-                for (int coordX = coordX1; coordX < coordX2 + 1; coordX++) {
-                    for (int coordZ = coordZ1; coordZ < coordZ2 + 1; coordZ++) {
-                        Block nearBlock = this.getLevel().getBlock(coordX, floorY, coordZ);
-                        if (nearBlock instanceof BlockIceFrosted) {
-                            nearFrosted.add(nearBlock);
-                        }
-
-                    }
-                }
-                int age = this.getDamage();
-                if ((new NukkitRandom()).nextRange(0, 2) == 0 || nearFrosted.size() < 4) {
-                    if (age >= 1) {
-                        age--;
-                        this.setDamage(age);
-                    }
-                }
-                if (age + 1 > 3) {
-                    this.getLevel().setBlock(this, new BlockWater(), true);
-                    if (nearFrosted.size() < 2) {
-                        for (Block iceBlock : nearFrosted) {
-                            this.getLevel().setBlock(iceBlock, new BlockWater(), true);
-                        }
-                    } else {
-                        for (Block iceBlock : nearFrosted) {
-                            iceBlock.setDamage(iceBlock.getDamage() + 1);
-                        }
-                    }
-                } else {
-                    this.setDamage(age + 1);
-                }
-                return Level.BLOCK_UPDATE_RANDOM;
-            }
-        }
-        return 0;
-    }
+    //@Override
+    //public int onUpdate(int type) {
+        //TODO: melt
+    //}
 
     @Override
     public Item toItem() {
