e47fd15a77f46dc2a995da0e395f28b088800b1c
Wode <wode490390@qq.com>
2018-10-30 11:32:44 +0800

    Revert LevelTimings.java

--- src/main/java/cn/nukkit/timings/LevelTimings.java
+++ src/main/java/cn/nukkit/timings/LevelTimings.java
@@ -17,7 +17,6 @@ public class LevelTimings {
     public final Timing tickChunks;
     public final Timing entityTick;
     public final Timing blockEntityTick;
-    public final Timing blockUpdate;
 
     public final Timing syncChunkSendTimer;
     public final Timing syncChunkSendPrepareTimer;
@@ -37,7 +36,6 @@ public class LevelTimings {
         this.tickChunks = TimingsManager.getTiming(name + "tickChunks");
         this.entityTick = TimingsManager.getTiming(name + "entityTick");
         this.blockEntityTick = TimingsManager.getTiming(name + "blockEntityTick");
-        this.blockUpdate = TimingsManager.getTiming(name + "blockUpdate");
 
         this.syncChunkSendTimer = TimingsManager.getTiming(name + "syncChunkSend");
         this.syncChunkSendPrepareTimer = TimingsManager.getTiming(name + "syncChunkSendPrepare");
