0d5500afefe003b7a24247692333a2d9564cf788
Wode <wode490390@qq.com>
2018-11-03 06:15:01 +0800

    stuff

--- src/main/java/cn/nukkit/inventory/InventoryType.java
+++ src/main/java/cn/nukkit/inventory/InventoryType.java
@@ -5,23 +5,32 @@ package cn.nukkit.inventory;
  * Nukkit Project
  */
 public enum InventoryType {
+    
+    PLAYER(40, "Player", -1), //36 CONTAINER, 4 ARMOR
+    CURSOR(1, "Cursor", -1),
+    OFFHAND(1, "Offhand", -1),
     CHEST(27, "Chest", 0),
     ENDER_CHEST(27, "Ender Chest", 0),
+    SHULKER_BOX(27, "Shulker Box", 0),
     DOUBLE_CHEST(27 + 27, "Double Chest", 0),
-    PLAYER(40, "Player", -1), //36 CONTAINER, 4 ARMOR
-    FURNACE(3, "Furnace", 2),
     CRAFTING(5, "Crafting", 1), //4 CRAFTING slots, 1 RESULT
     WORKBENCH(10, "Crafting", 1), //9 CRAFTING slots, 1 RESULT
+    FURNACE(3, "Furnace", 2),
+    ENCHANT_TABLE(2, "Enchant", 3), //1 INPUT/OUTPUT, 1 LAPIS
     BREWING_STAND(5, "Brewing", 4), //1 INPUT, 3 POTION, 1 fuel
     ANVIL(3, "Anvil", 5), //2 INPUT, 1 OUTPUT
-    ENCHANT_TABLE(2, "Enchant", 3), //1 INPUT/OUTPUT, 1 LAPIS
     DISPENSER(0, "Dispenser", 6), //9 CONTAINER
     DROPPER(9, "Dropper", 7), //9 CONTAINER
     HOPPER(5, "Hopper", 8), //5 CONTAINER
-    CURSOR(1, "Cursor", -1),
-    SHULKER_BOX(27, "Shulker Box", 0),
+    CAULDRON(1, "Cauldron", 9),
+    MINECART_CHEST(27, "Minecart Chest", 10),
+    MINECART_HOPPER(5, "Minecart Hopper", 11),
+    HORSE(2, "Horse", 12),
     BEACON(1, "Beacon", 13),
-    OFFHAND(1, "Offhand", -1);
+    //STRUCTURE_EDITOR(1, "Structure Editor", 14),
+    TRADING(3, "Trading", 15);
+    //COMMAND_BLOCK(1, "Command Block", 16),
+    //JUKEBOX(1, "Jukebox", 17);
 
     private final int size;
     private final String title;
