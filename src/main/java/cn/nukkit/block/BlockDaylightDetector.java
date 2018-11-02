5700aa5d6311ad054624ecef782f8d501cd97f18
Wode <wode490390@qq.com>
2018-11-03 02:28:14 +0800

    import ItemTool

--- src/main/java/cn/nukkit/block/BlockDaylightDetector.java
+++ src/main/java/cn/nukkit/block/BlockDaylightDetector.java
@@ -3,6 +3,7 @@ package cn.nukkit.block;
 import cn.nukkit.Player;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemBlock;
+import cn.nukkit.item.ItemTool;
 import cn.nukkit.utils.BlockColor;
 
 /**
