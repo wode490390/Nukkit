5b83449742cba4d678ab93d10364ae65eccf34f0
Wode <17339354+wode490390@users.noreply.github.com>
2018-11-25 19:27:30 +0800

    Update TextPacket.java

--- src/main/java/cn/nukkit/network/protocol/TextPacket.java
+++ src/main/java/cn/nukkit/network/protocol/TextPacket.java
@@ -16,28 +16,29 @@ public class TextPacket extends DataPacket {
     public static final byte TYPE_CHAT = 1;
     public static final byte TYPE_TRANSLATION = 2;
     public static final byte TYPE_POPUP = 3;
-    public static final byte TYPE_TIP = 4;
-    public static final byte TYPE_SYSTEM = 5;
-    public static final byte TYPE_WHISPER = 6;
-    public static final byte TYPE_ANNOUNCEMENT = 7;
+    public static final byte TYPE_JUKEBOX_POPUP = 4;
+    public static final byte TYPE_TIP = 5;
+    public static final byte TYPE_SYSTEM = 6;
+    public static final byte TYPE_WHISPER = 7;
+    public static final byte TYPE_ANNOUNCEMENT = 8;
 
     public byte type;
-    public String source = "";
+    public boolean needsTranslation = false;
+    public String sourceName = "";
     public String message = "";
     public String[] parameters = new String[0];
-    public boolean isLocalized = false;
+    public String xboxUserId = "";
     public String platformChatId = "";
 
     @Override
     public void decode() {
         this.type = (byte) getByte();
-        this.isLocalized = this.getBoolean() || type == TYPE_TRANSLATION;
+        this.needsTranslation = this.getBoolean() || type == TYPE_TRANSLATION;
         switch (type) {
-            case TYPE_POPUP:
             case TYPE_CHAT:
             case TYPE_WHISPER:
             case TYPE_ANNOUNCEMENT:
-                this.source = this.getString();
+                this.sourceName = this.getString();
             case TYPE_RAW:
             case TYPE_TIP:
             case TYPE_SYSTEM:
@@ -45,13 +46,18 @@ public class TextPacket extends DataPacket {
                 break;
 
             case TYPE_TRANSLATION:
+            case TYPE_POPUP:
+            case TYPE_JUKEBOX_POPUP:
                 this.message = this.getString();
                 int count = (int) this.getUnsignedVarInt();
                 this.parameters = new String[count];
                 for (int i = 0; i < count; i++) {
                     this.parameters[i] = this.getString();
                 }
+                break;
         }
+
+        this.xboxUserId = this.getString();
         this.platformChatId = this.getString();
     }
 
@@ -59,13 +65,12 @@ public class TextPacket extends DataPacket {
     public void encode() {
         this.reset();
         this.putByte(this.type);
-        this.putBoolean(this.isLocalized || type == TYPE_TRANSLATION);
+        this.putBoolean(this.needsTranslation || type == TYPE_TRANSLATION);
         switch (this.type) {
-            case TYPE_POPUP:
             case TYPE_CHAT:
             case TYPE_WHISPER:
             case TYPE_ANNOUNCEMENT:
-                this.putString(this.source);
+                this.putString(this.sourceName);
             case TYPE_RAW:
             case TYPE_TIP:
             case TYPE_SYSTEM:
@@ -73,13 +78,17 @@ public class TextPacket extends DataPacket {
                 break;
 
             case TYPE_TRANSLATION:
+            case TYPE_POPUP:
+            case TYPE_JUKEBOX_POPUP:
                 this.putString(this.message);
                 this.putUnsignedVarInt(this.parameters.length);
-                for (String parameter : this.parameters) {
-                    this.putString(parameter);
+                for (String p : this.parameters) {
+                    this.putString(p);
                 }
+                break;
         }
+
+        this.putString(this.xboxUserId);
         this.putString(this.platformChatId);
     }
-
 }
