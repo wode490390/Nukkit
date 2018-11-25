c211be132351520e2cd1cbc90f9ae3d021839c57
Wode <17339354+wode490390@users.noreply.github.com>
2018-11-25 22:25:17 +0800

    Update PlayerListEntry.java

--- src/main/java/cn/nukkit/network/protocol/types/PlayerListEntry.java
+++ src/main/java/cn/nukkit/network/protocol/types/PlayerListEntry.java
@@ -6,35 +6,31 @@ import java.util.UUID;
 
 public class PlayerListEntry {
 
-    public UUID uuid;
-    public long entityUniqueId;
-    public String username;
-    public Skin skin;
-    public String xboxUserId;
-    public String platformChatId;
+    public final UUID uuid;
+    public long entityUniqueId = 0;
+    public String username = "";
+    public Skin skin = new Skin();
+    public String xboxUserId = "";
+    public String platformChatId = "";
 
-    public PlayerListEntry(UUID uuid, int entityUniqueId, String username, Skin skin, String xboxUserId, String platformChatId) {
+    public PlayerListEntry(UUID uuid) { //createRemovalEntry
+        this.uuid = uuid;
+    }
+
+    public PlayerListEntry(UUID uuid, int entityUniqueId, String username, Skin skin) {
+        this(uuid, entityUniqueId, username, skin, "");
+    }
+
+    public PlayerListEntry(UUID uuid, int entityUniqueId, String username, Skin skin, String xboxUserId) {
+        this(uuid, entityUniqueId, username, skin, xboxUserId, "");
+    }
+
+    public PlayerListEntry(UUID uuid, int entityUniqueId, String username, Skin skin, String xboxUserId, String platformChatId) { //createAdditionEntry
         this.uuid = uuid;
         this.entityUniqueId = entityUniqueId;
         this.username = username;
         this.skin = skin;
         this.xboxUserId = xboxUserId;
         this.platformChatId = platformChatId;
-    }
-
-    public static PlayerListEntry createAdditionEntry(UUID uuid, int entityUniqueId, String username, Skin skin) {
-        return createAdditionEntry(uuid, entityUniqueId, username, skin, "");
-    }
-
-    public static PlayerListEntry createAdditionEntry(UUID uuid, int entityUniqueId, String username, Skin skin, String xboxUserId) {
-        return createAdditionEntry(uuid, entityUniqueId, username, skin, xboxUserId, "");
-    }
-
-    public static PlayerListEntry createAdditionEntry(UUID uuid, int entityUniqueId, String username, Skin skin, String xboxUserId, String platformChatId) {
-        return new PlayerListEntry(uuid, entityUniqueId, username, skin, xboxUserId, platformChatId);
-    }
-
-    public static PlayerListEntry createRemovalEntry(UUID uuid) {
-        return new PlayerListEntry(uuid, -1, null, null, "", "");
     }
 }
