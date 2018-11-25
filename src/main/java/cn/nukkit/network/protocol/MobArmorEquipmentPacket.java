b0b60d65ef2985bf0f486db1515dafee06624d84
Wode <17339354+wode490390@users.noreply.github.com>
2018-11-26 03:47:47 +0800

    Update MobArmorEquipmentPacket.java

--- src/main/java/cn/nukkit/network/protocol/MobArmorEquipmentPacket.java
+++ src/main/java/cn/nukkit/network/protocol/MobArmorEquipmentPacket.java
@@ -22,7 +22,7 @@ public class MobArmorEquipmentPacket extends DataPacket {
     public void decode() {
         this.entityRuntimeId = this.getEntityRuntimeId();
         this.slots = new Item[4];
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; ++i) {
             this.slots[i] = this.getSlot();
         }
     }
@@ -31,7 +31,7 @@ public class MobArmorEquipmentPacket extends DataPacket {
     public void encode() {
         this.reset();
         this.putEntityRuntimeId(this.entityRuntimeId);
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; ++i) {
             this.putSlot(this.slots[i]);
         }
     }
