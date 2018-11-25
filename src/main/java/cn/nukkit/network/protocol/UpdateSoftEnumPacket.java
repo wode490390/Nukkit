c988b4bbab6ada185ea24d073eabfe9bc2e55baa
Wode <17339354+wode490390@users.noreply.github.com>
2018-11-25 18:58:58 +0800

    Update UpdateSoftEnumPacket.java

--- src/main/java/cn/nukkit/network/protocol/UpdateSoftEnumPacket.java
+++ src/main/java/cn/nukkit/network/protocol/UpdateSoftEnumPacket.java
@@ -1,10 +1,19 @@
 package cn.nukkit.network.protocol;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UpdateSoftEnumPacket extends DataPacket {
 
-    public final String[] values = new String[0];
-    public String name = "";
-    public Type type = Type.SET;
+    public static final byte NETWORK_ID = ProtocolInfo.UPDATE_SOFT_ENUM_PACKET;
+
+    public static final int TYPE_ADD = 0;
+    public static final int TYPE_REMOVE = 1;
+    public static final int TYPE_SET = 2;
+
+    public String enumName = "";
+    public List<String> values = new ArrayList<String>();
+    public int type;
 
     @Override
     public byte pid() {
@@ -18,18 +27,11 @@ public class UpdateSoftEnumPacket extends DataPacket {
     @Override
     public void encode() {
         this.reset();
-        this.putString(name);
-        this.putUnsignedVarInt(values.length);
-
-        for (String value : values) {
+        this.putString(this.enumName);
+        this.putUnsignedVarInt(this.values.length);
+        for (String value : this.values) {
             this.putString(value);
         }
-        this.putByte((byte) type.ordinal());
-    }
-
-    public enum Type {
-        ADD,
-        REMOVE,
-        SET
+        this.putByte((byte) this.type);
     }
 }
