aaddffd502e56e0b5a1775c2ed57384e4384ff76
Wode <17339354+wode490390@users.noreply.github.com>
2018-11-25 18:13:23 +0800

    Oops

--- src/main/java/cn/nukkit/network/protocol/types/ScorePacketEntry.java
+++ src/main/java/cn/nukkit/network/protocol/types/ScorePacketEntry.java
@@ -7,7 +7,7 @@ public class ScorePacketEntry {
     public static final int TYPE_ENTITY = 2;
     public static final int TYPE_FAKE_PLAYER = 3;
 
-    public int scoreboardId;
+    public long scoreboardId;
     public String objectiveName;
     public int score;
 
