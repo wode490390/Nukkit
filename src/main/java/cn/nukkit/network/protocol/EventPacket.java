f9265fe88748b9e26d80d077f36cb08cb74b510d
Wode <17339354+wode490390@users.noreply.github.com>
2018-11-26 04:09:35 +0800

    Update EventPacket.java

--- src/main/java/cn/nukkit/network/protocol/EventPacket.java
+++ src/main/java/cn/nukkit/network/protocol/EventPacket.java
@@ -2,13 +2,29 @@ package cn.nukkit.network.protocol;
 
 public class EventPacket extends DataPacket {
 
-    public long eid;
-    public int unknown1;
-    public byte unknown2;
+    public static final byte NETWORK_ID = ProtocolInfo.EVENT_PACKET;
+
+    public static final int TYPE_ACHIEVEMENT_AWARDED = 0;
+    public static final int TYPE_ENTITY_INTERACT = 1;
+    public static final int TYPE_PORTAL_BUILT = 2;
+    public static final int TYPE_PORTAL_USED = 3;
+    public static final int TYPE_MOB_KILLED = 4;
+    public static final int TYPE_CAULDRON_USED = 5;
+    public static final int TYPE_PLAYER_DEATH = 6;
+    public static final int TYPE_BOSS_KILLED = 7;
+    public static final int TYPE_AGENT_COMMAND = 8;
+    public static final int TYPE_AGENT_CREATED = 9;
+    public static final int TYPE_PATTERN_REMOVED = 10; //???
+    public static final int TYPE_COMMANED_EXECUTED = 11;
+    public static final int TYPE_FISH_BUCKETED = 12;
+
+    public long playerRuntimeId;
+    public int eventData;
+    public byte type;
 
     @Override
     public byte pid() {
-        return ProtocolInfo.EVENT_PACKET;
+        return NETWORK_ID;
     }
 
     @Override
@@ -19,8 +35,10 @@ public class EventPacket extends DataPacket {
     @Override
     public void encode() {
         this.reset();
-        this.putVarLong(this.eid);
-        this.putVarInt(this.unknown1);
-        this.putByte(this.unknown2);
+        this.putEntityRuntimeId(this.playerRuntimeId);
+        this.putVarInt(this.eventData);
+        this.putByte(this.type);
+
+        //TODO: nice confusing mess
     }
 }
