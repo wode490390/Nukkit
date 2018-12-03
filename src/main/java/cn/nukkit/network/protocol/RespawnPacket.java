7557e1f35431f26950b9dececbaefb6d2911dc7c
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-03 14:27:08 +0800

    Update RespawnPacket.java

--- src/main/java/cn/nukkit/network/protocol/RespawnPacket.java
+++ src/main/java/cn/nukkit/network/protocol/RespawnPacket.java
@@ -1,6 +1,6 @@
 package cn.nukkit.network.protocol;
 
-import cn.nukkit.math.Vector3;
+import cn.nukkit.math.Vector3f;
 
 /**
  * @author Nukkit Project Team
@@ -9,17 +9,17 @@ public class RespawnPacket extends DataPacket {
 
     public static final byte NETWORK_ID = ProtocolInfo.RESPAWN_PACKET;
 
-    public Vector3 position;
+    public Vector3f position;
 
     @Override
     public void decode() {
-        this.position = this.getVector3().asVector3();
+        this.position = this.getVector3();
     }
 
     @Override
     public void encode() {
         this.reset();
-        this.putVector3(this.position.asVector3f());
+        this.putVector3(this.position);
     }
 
     @Override
