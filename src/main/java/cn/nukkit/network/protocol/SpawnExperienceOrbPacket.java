8ec39135289952036e4f4aa05a94fc98a6c71d00
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-03 14:30:09 +0800

    Update SpawnExperienceOrbPacket.java

--- src/main/java/cn/nukkit/network/protocol/SpawnExperienceOrbPacket.java
+++ src/main/java/cn/nukkit/network/protocol/SpawnExperienceOrbPacket.java
@@ -1,12 +1,12 @@
 package cn.nukkit.network.protocol;
 
-import cn.nukkit.math.Vector3;
+import cn.nukkit.math.Vector3f;
 
 public class SpawnExperienceOrbPacket extends DataPacket {
 
     public static final byte NETWORK_ID = ProtocolInfo.SPAWN_EXPERIENCE_ORB_PACKET;
 
-    public Vector3 position;
+    public Vector3f position;
     public int amount;
 
     @Override
@@ -17,7 +17,7 @@ public class SpawnExperienceOrbPacket extends DataPacket {
     @Override
     public void encode() {
         this.reset();
-        this.putVector3(this.position.asVector3f());
+        this.putVector3(this.position);
         this.putVarInt(this.amount);
     }
 
