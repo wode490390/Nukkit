9b3d5d854f1f38cd325c920da34fc51ed6200bf2
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-04 15:11:01 +0800

    Update UpdateAttributesPacket.java

--- src/main/java/cn/nukkit/network/protocol/UpdateAttributesPacket.java
+++ src/main/java/cn/nukkit/network/protocol/UpdateAttributesPacket.java
@@ -23,20 +23,7 @@ public class UpdateAttributesPacket extends DataPacket {
 
     public void encode() {
         this.reset();
-
         this.putEntityRuntimeId(this.entityRuntimeId);
-
-        if (this.entries == null) {
-            this.putUnsignedVarInt(0);
-        } else {
-            this.putUnsignedVarInt(this.entries.length);
-            for (Attribute entry : this.entries) {
-                this.putLFloat(entry.getMinValue());
-                this.putLFloat(entry.getMaxValue());
-                this.putLFloat(entry.getValue());
-                this.putLFloat(entry.getDefaultValue());
-                this.putString(entry.getName());
-            }
-        }
+        this.putAttributeList(this.entries);
     }
 }
