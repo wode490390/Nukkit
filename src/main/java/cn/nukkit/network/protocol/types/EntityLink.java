56d37ea3e938fb802b779de8ace46e8e6b480549
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-04 17:56:00 +0800

    Update EntityLink.java

--- src/main/java/cn/nukkit/network/protocol/types/EntityLink.java
+++ src/main/java/cn/nukkit/network/protocol/types/EntityLink.java
@@ -6,10 +6,22 @@ public class EntityLink {
     public static final int TYPE_RIDER = 1;
     public static final int TYPE_PASSENGER = 2;
 
-    public long fromEntityUniqueId;
-    public long toEntityUniqueId;
-    public int type;
-    public boolean immediate; //for dismounting on mount death
+    public long fromEntityUniqueId = 0;
+    public long toEntityUniqueId = 0;
+    public int type = 0;
+    public boolean immediate = false; //for dismounting on mount death
+
+    public EntityLink() {
+        this(0)
+    }
+
+    public EntityLink(long fromEntityUniqueId) {
+        this(fromEntityUniqueId, 0)
+    }
+
+    public EntityLink(long fromEntityUniqueId, long toEntityUniqueId) {
+        this(fromEntityUniqueId, toEntityUniqueId, 0)
+    }
 
     public EntityLink(long fromEntityUniqueId, long toEntityUniqueId, int type) {
         this(fromEntityUniqueId, toEntityUniqueId, type, false)
