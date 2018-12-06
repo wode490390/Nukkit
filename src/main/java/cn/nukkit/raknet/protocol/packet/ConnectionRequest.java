e3bc60483652929afce9762e3bab3754d6c8aa5a
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-06 21:36:12 +0800

    Update and rename CLIENT_CONNECT_DataPacket.java to ConnectionRequest.java

--- src/main/java/cn/nukkit/raknet/protocol/packet/ConnectionRequest.java
+++ src/main/java/cn/nukkit/raknet/protocol/packet/ConnectionRequest.java
@@ -6,7 +6,7 @@ import cn.nukkit.raknet.protocol.Packet;
  * author: MagicDroidX
  * Nukkit Project
  */
-public class CLIENT_CONNECT_DataPacket extends Packet {
+public class ConnectionRequest extends Packet {
     public static final byte ID = (byte) 0x09;
 
     @Override
@@ -15,14 +15,14 @@ public class CLIENT_CONNECT_DataPacket extends Packet {
     }
 
     public long clientID;
-    public long sendPing;
+    public long sendPingTime;
     public boolean useSecurity = false;
 
     @Override
     public void encode() {
         super.encode();
         this.putLong(this.clientID);
-        this.putLong(this.sendPing);
+        this.putLong(this.sendPingTime);
         this.putByte((byte) (this.useSecurity ? 1 : 0));
     }
 
@@ -30,15 +30,15 @@ public class CLIENT_CONNECT_DataPacket extends Packet {
     public void decode() {
         super.decode();
         this.clientID = this.getLong();
-        this.sendPing = this.getLong();
-        this.useSecurity = this.getByte() > 0;
+        this.sendPingTime = this.getLong();
+        this.useSecurity = (this.getByte() != 0);
     }
 
     public static final class Factory implements Packet.PacketFactory {
 
         @Override
         public Packet create() {
-            return new CLIENT_CONNECT_DataPacket();
+            return new ConnectionRequest();
         }
 
     }
