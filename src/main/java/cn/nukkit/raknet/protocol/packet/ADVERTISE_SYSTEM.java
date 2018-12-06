cfcc08a8eb385d2135b7414c4d4fcad7b070a95f
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-06 21:28:54 +0800

    Update ADVERTISE_SYSTEM.java

--- src/main/java/cn/nukkit/raknet/protocol/packet/ADVERTISE_SYSTEM.java
+++ src/main/java/cn/nukkit/raknet/protocol/packet/ADVERTISE_SYSTEM.java
@@ -1,17 +1,33 @@
 package cn.nukkit.raknet.protocol.packet;
 
+import cn.nukkit.raknet.protocol.MessageIdentifiers;
 import cn.nukkit.raknet.protocol.Packet;
 
 /**
  * author: MagicDroidX
  * Nukkit Project
  */
-public class ADVERTISE_SYSTEM extends UNCONNECTED_PONG {
-    public static final byte ID = (byte) 0x1d;
+public class ADVERTISE_SYSTEM extends Packet {
+
+    public static final byte ID = MessageIdentifiers.ID_ADVERTISE_SYSTEM;
+
+    public String serverName;
 
     @Override
     public byte getID() {
         return ID;
+    }
+
+    @Override
+    public void encode() {
+        super.encode();
+        this.putString(this.serverName);
+    }
+
+    @Override
+    public void decode() {
+        super.decode();
+        this.serverName = this.getString();
     }
 
     public static final class Factory implements Packet.PacketFactory {
