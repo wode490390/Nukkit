9debfb5bb56544c4098258066b1b8b7eda6af493
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-07 02:35:10 +0800

    Update and rename UNCONNECTED_PING.java to UnconnectedPing.java

--- src/main/java/cn/nukkit/raknet/protocol/packet/UnconnectedPing.java
+++ src/main/java/cn/nukkit/raknet/protocol/packet/UnconnectedPing.java
@@ -1,14 +1,16 @@
 package cn.nukkit.raknet.protocol.packet;
 
-import cn.nukkit.raknet.RakNet;
+import cn.nukkit.raknet.protocol.MessageIdentifiers;
+import cn.nukkit.raknet.protocol.OfflineMessage;
 import cn.nukkit.raknet.protocol.Packet;
 
 /**
  * author: MagicDroidX
  * Nukkit Project
  */
-public class UNCONNECTED_PING extends Packet {
-    public static final byte ID = (byte) 0x01;
+public class UnconnectedPing extends OfflineMessage {
+
+    public static final byte ID = MessageIdentifiers.ID_UNCONNECTED_PING;
 
     @Override
     public byte getID() {
@@ -21,20 +23,21 @@ public class UNCONNECTED_PING extends Packet {
     public void encode() {
         super.encode();
         this.putLong(this.pingID);
-        this.put(RakNet.MAGIC);
+        this.writeMagic();
     }
 
     @Override
     public void decode() {
         super.decode();
         this.pingID = this.getLong();
+        this.readMagic();
     }
 
     public static final class Factory implements Packet.PacketFactory {
 
         @Override
         public Packet create() {
-            return new UNCONNECTED_PING();
+            return new UnconnectedPing();
         }
 
     }
