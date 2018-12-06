dc3dfd844287c269767aa477f78091ef3f148f41
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-07 01:04:54 +0800

    Update Datagram.java

--- src/main/java/cn/nukkit/raknet/protocol/Datagram.java
+++ src/main/java/cn/nukkit/raknet/protocol/Datagram.java
@@ -10,13 +10,27 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  */
 public abstract class Datagram extends Packet {
 
+    public static final byte BITFLAG_VALID = (byte) 0x80;
+    public static final byte BITFLAG_ACK = (byte) 0x40;
+    public static final byte BITFLAG_NAK = (byte) 0x20; // hasBAndAS for ACKs
+
+    /*
+     * These flags can be set on regular datagrams, but they are useless as per the public version of RakNet
+     * (the receiving client will not use them or pay any attention to them).
+     */
+    public static final byte BITFLAG_PACKET_PAIR = (byte) 0x10;
+    public static final byte BITFLAG_CONTINUOUS_SEND = (byte) 0x08;
+    public static final byte BITFLAG_NEEDS_B_AND_AS = (byte) 0x04;
+
+    public byte headerFlags = 0;
+
     public ConcurrentLinkedQueue<Object> packets = new ConcurrentLinkedQueue<>();
 
     public Integer seqNumber;
 
     @Override
     public void encode() {
-        super.encode();
+        this.putByte(BITFLAG_VALID | this.headerFlags);
         this.putLTriad(this.seqNumber);
         for (Object packet : this.packets) {
             this.put(packet instanceof EncapsulatedPacket ? ((EncapsulatedPacket) packet).toBinary() : (byte[]) packet);
@@ -34,7 +48,7 @@ public abstract class Datagram extends Packet {
 
     @Override
     public void decode() {
-        super.decode();
+        this.headerFlags = this.getByte();
         this.seqNumber = this.getLTriad();
 
         while (!this.feof()) {
@@ -52,6 +66,7 @@ public abstract class Datagram extends Packet {
     public Packet clean() {
         this.packets = new ConcurrentLinkedQueue<>();
         this.seqNumber = null;
+
         return super.clean();
     }
 
