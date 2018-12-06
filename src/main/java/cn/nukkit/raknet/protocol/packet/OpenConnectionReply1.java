b635d2022b53633afa366b1695bd3f4dec2e35ec
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-07 02:12:25 +0800

    Update and rename OPEN_CONNECTION_REPLY_1.java to OpenConnectionReply1.java

--- src/main/java/cn/nukkit/raknet/protocol/packet/OpenConnectionReply1.java
+++ src/main/java/cn/nukkit/raknet/protocol/packet/OpenConnectionReply1.java
@@ -1,14 +1,16 @@
 package cn.nukkit.raknet.protocol.packet;
 
-import cn.nukkit.raknet.RakNet;
+import cn.nukkit.raknet.protocol.MessageIdentifiers;
+import cn.nukkit.raknet.protocol.OfflineMessage;
 import cn.nukkit.raknet.protocol.Packet;
 
 /**
  * author: MagicDroidX
  * Nukkit Project
  */
-public class OPEN_CONNECTION_REPLY_1 extends Packet {
-    public static final byte ID = (byte) 0x06;
+public class OpenConnectionReply1 extends OfflineMessage {
+
+    public static final byte ID = MessageIdentifiers.ID_OPEN_CONNECTION_REPLY_1;
 
     @Override
     public byte getID() {
@@ -16,31 +18,32 @@ public class OPEN_CONNECTION_REPLY_1 extends Packet {
     }
 
     public long serverID;
+    public boolean serverSecurity = false;
     public short mtuSize;
 
     @Override
     public void encode() {
         super.encode();
-        this.put(RakNet.MAGIC);
+        this.writeMagic();
         this.putLong(this.serverID);
-        this.putByte((byte) 0); //server security
+        this.putByte((byte) (this.serverSecurity ? 1 : 0));
         this.putShort(this.mtuSize);
     }
 
     @Override
     public void decode() {
         super.decode();
-        this.offset += 16; //skip magic bytes
+        this.readMagic();
         this.serverID = this.getLong();
-        this.getByte(); //skip security
-        this.mtuSize = this.getSignedShort();
+        this.serverSecurity = (this.getByte() != 0);
+        this.mtuSize = this.getShort();
     }
 
     public static final class Factory implements Packet.PacketFactory {
 
         @Override
         public Packet create() {
-            return new OPEN_CONNECTION_REPLY_1();
+            return new OpenConnectionReply1();
         }
 
     }
