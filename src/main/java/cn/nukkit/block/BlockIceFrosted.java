a32deb2eb66ed94fa10c7c260b97e84dff48599d
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-09 23:45:46 +0800

    oops

--- src/main/java/cn/nukkit/block/BlockIceFrosted.java
+++ src/main/java/cn/nukkit/block/BlockIceFrosted.java
@@ -2,6 +2,7 @@ package cn.nukkit.block;
 
 import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemBlock;
+import cn.nukkit.item.ItemTool;
 import cn.nukkit.level.Level;
 import cn.nukkit.utils.BlockColor;
 
