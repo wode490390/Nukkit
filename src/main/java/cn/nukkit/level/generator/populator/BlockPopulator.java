cebaaf01e338da8150801a092aa2986343ad4a37
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-12 10:14:17 +0800

    Add BlockPopulator

---
+++ src/main/java/cn/nukkit/level/generator/populator/BlockPopulator.java
@@ -0,0 +1,23 @@
+package cn.nukkit.level.generator.populator;
+
+import cn.nukkit.level.ChunkManager;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.NukkitRandom;
+
+public abstract class BlockPopulator extends Populator {
+
+    protected int amount;
+
+    public final void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public abstract void decorate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, FullChunk chunk);
+
+    @Override
+    public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, FullChunk chunk) {
+        for (int i = 0; i < amount; i++) {
+            decorate(level, chunkX, chunkZ, random, chunk);
+        }
+    }
+}
