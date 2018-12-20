2c7ed43f909d849fdf215b616473545a22af0cd2
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-20 20:05:53 +0800

    Rename

--- src/main/java/cn/nukkit/item/enchantment/curse/EnchantmentCurseVanishing.java
+++ src/main/java/cn/nukkit/item/enchantment/curse/EnchantmentCurseVanishing.java
@@ -6,6 +6,6 @@ import cn.nukkit.item.enchantment.EnchantmentType;
 public class EnchantmentCurseVanishing extends EnchantmentCurse {
 
     public EnchantmentCurseVanishing() {
-        super(Enchantment.ID_VANISHING_CURSE, "vanishing", EnchantmentType.ALL);
+        super(Enchantment.ID_CURSE_VANISHING, "vanishing", EnchantmentType.ALL);
     }
 }
