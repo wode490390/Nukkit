17b3c753b4f1a8089616d9dc3dbd9962d3ca94e2
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-21 11:39:07 +0800

    oops

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorObsidianPillar.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorObsidianPillar.java
@@ -15,7 +15,7 @@ public class PopulatorObsidianPillar extends BlockPopulator {
             int z = (chunkZ << 4) + random.nextBoundedInt(16);
             int y = this.getHighestWorkableBlock(level, x, z, chunk);
 
-            if (level.getBlockId(x, y, z) != 0 || level.getBlockId(x, y - 1, z) != END_STONE) {
+            if (chunk.getBlockId(x, y, z) != 0 || chunk.getBlockId(x, y - 1, z) != END_STONE) {
                 return;
             }
 
@@ -25,7 +25,7 @@ public class PopulatorObsidianPillar extends BlockPopulator {
             // check under the pillar that there's no gap
             for (int i = -radius; i <= radius; i++) {
                 for (int j = -radius; j <= radius; j++) {
-                    if (i * i + j * j <= radius * radius + 1 && level.getBlockId(x + i, y - 1, z + j) != END_STONE) {
+                    if (i * i + j * j <= radius * radius + 1 && chunk.getBlockId(x + i, y - 1, z + j) != END_STONE) {
                         return;
                     }
                 }
@@ -36,14 +36,14 @@ public class PopulatorObsidianPillar extends BlockPopulator {
                 for (int i = -radius; i <= radius; i++) {
                     for (int j = -radius; j <= radius; j++) {
                         if (i * i + j * j <= radius * radius + 1) {
-                            level.setBlock(x + i, y + k, z + j, OBSIDIAN);
+                            chunk.setBlock(x + i, y + k, z + j, OBSIDIAN);
                         }
                     }
                 }
             }
 
             //TODO: level.spawnEntity(new EntityEndCrystal(), x + 0.5D, y + height + 1, z + 0.5D, random.nextFloat() * 360, 0);
-            level.setBlock(x, y + height, z, BEDROCK);
+            chunk.setBlock(x, y + height, z, BEDROCK);
         }
     }
 }
