622ac54ca2ce51c7f382bdc357c5f01a3d1179f6
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-21 14:08:00 +0800

    Fix IAE

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorObsidianPillar.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorObsidianPillar.java
@@ -18,7 +18,7 @@ public class PopulatorObsidianPillar extends BlockPopulator {
                 return;
             }
 
-            if (chunk.getBlockId(x, y, z) != 0 || chunk.getBlockId(x, y - 1, z) != END_STONE) {
+            if (level.getBlockIdAt(x, y, z) != 0 || level.getBlockIdAt(x, y - 1, z) != END_STONE) {
                 return;
             }
 
@@ -28,7 +28,7 @@ public class PopulatorObsidianPillar extends BlockPopulator {
             // check under the pillar that there's no gap
             for (int i = -radius; i <= radius; i++) {
                 for (int j = -radius; j <= radius; j++) {
-                    if (i * i + j * j <= radius * radius + 1 && chunk.getBlockId(x + i, y - 1, z + j) != END_STONE) {
+                    if (i * i + j * j <= radius * radius + 1 && level.getBlockIdAt(x + i, y - 1, z + j) != END_STONE) {
                         return;
                     }
                 }
@@ -39,14 +39,14 @@ public class PopulatorObsidianPillar extends BlockPopulator {
                 for (int i = -radius; i <= radius; i++) {
                     for (int j = -radius; j <= radius; j++) {
                         if (i * i + j * j <= radius * radius + 1) {
-                            chunk.setBlock(x + i, y + k, z + j, OBSIDIAN);
+                            level.setBlockAt(x + i, y + k, z + j, OBSIDIAN);
                         }
                     }
                 }
             }
 
             //TODO: level.spawnEntity(new EntityEndCrystal(), x + 0.5D, y + height + 1, z + 0.5D, random.nextFloat() * 360, 0);
-            chunk.setBlock(x, y + height, z, BEDROCK);
+            level.setBlockAt(x, y + height, z, BEDROCK);
         }
     }
 }
