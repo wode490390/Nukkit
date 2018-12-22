3fd12d2a530038ad7e207bc871179d5e31d49c62
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-23 00:39:34 +0800

    Feature

--- src/main/java/cn/nukkit/level/generator/Generator.java
+++ src/main/java/cn/nukkit/level/generator/Generator.java
@@ -19,6 +19,7 @@ public abstract class Generator implements BlockID {
     public static final int TYPE_FLAT = 2;
     public static final int TYPE_NETHER = 3;
     public static final int TYPE_THE_END = 4;
+    public static final int TYPE_LARGE_BIOMES = 5;
 
     public abstract int getId();
 
