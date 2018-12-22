0fe511fbe0f720064a2ea5dbbd61681f9cee0446
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-22 20:22:41 +0800

    TODO: side check

--- src/main/java/cn/nukkit/block/BlockChorusFlower.java
+++ src/main/java/cn/nukkit/block/BlockChorusFlower.java
@@ -1,11 +1,22 @@
 package cn.nukkit.block;
 
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.event.block.BlockGrowEvent;
 import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemChorusFruit;
 import cn.nukkit.item.ItemTool;
+import cn.nukkit.level.Level;
+import cn.nukkit.math.BlockFace;
 
-public class BlockChorusFlower extends BlockTransparent {
+public class BlockChorusFlower extends BlockTransparentMeta {
 
     public BlockChorusFlower() {
+        super(0);
+    }
+
+    public BlockChorusFlower(int meta) {
+        super(meta);
     }
 
     @Override
@@ -35,6 +46,46 @@ public class BlockChorusFlower extends BlockTransparent {
 
     @Override
     public Item[] getDrops(Item item) {
-        return new Item[0];
+        return new Item[]{new ItemChorusFruit()};
+    }
+
+    @Override
+    public boolean place(Item item, Block block, Block target, BlockFace face, double fx, double fy, double fz, Player player) {
+        Block below = this.down();
+        if (below instanceof BlockEndStone || below instanceof BlockChorusPlant) {
+            this.getLevel().setBlock(block, this, true, true);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int onUpdate(int type) {
+        if (type == Level.BLOCK_UPDATE_NORMAL) {
+            Block below = this.down();
+            if (!(below instanceof BlockEndStone || below instanceof BlockChorusPlant)) {
+                this.getLevel().useBreakOn(this);
+                return Level.BLOCK_UPDATE_NORMAL;
+            }
+        } else if (type == Level.BLOCK_UPDATE_RANDOM) {
+            if (new Random().nextInt(3) == 1) {
+                if (this.getDamage() < 0x06) {
+                    BlockChorusFlower block = this.clone();
+                    block.setDamage(block.getDamage() + 1);
+                    BlockGrowEvent ev = new BlockGrowEvent(this, block);
+                    Server.getInstance().getPluginManager().callEvent(ev);
+
+                    if (!ev.isCancelled()) {
+                        this.getLevel().setBlock(this, ev.getNewState(), false, true);
+                    } else {
+                        return Level.BLOCK_UPDATE_RANDOM;
+                    }
+                }
+            } else {
+                return Level.BLOCK_UPDATE_RANDOM;
+            }
+        }
+
+        return 0;
     }
 }
