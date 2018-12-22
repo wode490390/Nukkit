11b58decd694445f36dc90178dc57caeba2e3265
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-22 18:27:46 +0800

    Fix face

--- src/main/java/cn/nukkit/block/BlockCommandBlock.java
+++ src/main/java/cn/nukkit/block/BlockCommandBlock.java
@@ -1,7 +1,9 @@
 package cn.nukkit.block;
 
+import cn.nukkit.Player;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemBlock;
+import cn.nukkit.math.BlockFace;
 
 public class BlockCommandBlock extends BlockSolidMeta {
 
@@ -51,5 +53,29 @@ public class BlockCommandBlock extends BlockSolidMeta {
     @Override
     public boolean isBreakable(Item item) {
         return false;
+    }
+
+    @Override
+    public boolean place(Item item, Block block, Block target, BlockFace face, double fx, double fy, double fz, Player player) {
+        int faces[] = {2, 5, 3, 4};
+        if (player != null) {
+            if (Math.abs(player.x - this.x) < 2 && Math.abs(player.z - this.z) < 2) {
+                double y = player.y + player.getEyeHeight();
+                if (y - this.y > 2) {
+                    this.setDamage(BlockFace.UP.getIndex());
+                } else if (this.y - y > 0) {
+                    this.setDamage(BlockFace.DOWN.getIndex());
+                } else {
+                    this.setDamage(player.getHorizontalFacing().getIndex());
+                }
+            } else {
+                this.setDamage(player.getHorizontalFacing().getIndex());
+            }
+        } else {
+            this.setDamage(0);
+        }
+        this.level.setBlock(block, this, true, false);
+
+        return true;
     }
 }
