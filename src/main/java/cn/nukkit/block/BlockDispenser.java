847425fbfa4c2c55da37fae1424030629277be91
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-22 21:19:06 +0800

    Try fix face

--- src/main/java/cn/nukkit/block/BlockDispenser.java
+++ src/main/java/cn/nukkit/block/BlockDispenser.java
@@ -1,5 +1,8 @@
 package cn.nukkit.block;
 
+import cn.nukkit.Player;
+//import cn.nukkit.blockentity.BlockEntity;
+//import cn.nukkit.blockentity.BlockEntityDispenser;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemBlock;
 import cn.nukkit.math.BlockFace;
@@ -73,6 +76,11 @@ public class BlockDispenser extends BlockSolidMeta {
         return false;
     }
 
+    @Override
+    public boolean canBeActivated() {
+        return true;
+    }
+
     public Vector3 getDispensePosition() {
         BlockFace facing = getFacing();
         double x = this.getX() + 0.7 * facing.getXOffset();
@@ -80,4 +88,51 @@ public class BlockDispenser extends BlockSolidMeta {
         double z = this.getZ() + 0.7 * facing.getZOffset();
         return new Vector3(x, y, z);
     }
+
+    @Override
+    public boolean place(Item item, Block block, Block target, BlockFace face, double fx, double fy, double fz, Player player) {
+        int faces[] = {2, 5, 3, 4};
+        if (player != null) {
+            if (Math.abs(player.x - this.x) < 2 && Math.abs(player.z - this.z) < 2) {
+                double y = player.y + player.getEyeHeight();
+                if (y - this.y > 2) {
+                    this.setDamage(BlockFace.UP.getIndex());
+                } else if (this.y - y > 0) {
+                    this.setDamage(BlockFace.DOWN.getIndex());
+                } else {
+                    this.setDamage(player.getHorizontalFacing().getIndex());
+                }
+            } else {
+                this.setDamage(player.getHorizontalFacing().getIndex());
+            }
+        } else {
+            this.setDamage(0);
+        }
+        this.level.setBlock(block, this, true, false);
+
+        return true;
+    }
+
+    /*@Override
+    public boolean onActivate(Item item, Player player) {
+        if (player != null) {
+            BlockEntity t = this.getLevel().getBlockEntity(this);
+            BlockEntityDispenser container;
+            if (t instanceof BlockEntityDispenser) {
+                container = (BlockEntityDispenser) t;
+            } else {
+                CompoundTag nbt = new CompoundTag("")
+                        .putList(new ListTag<>("Items"))
+                        .putString("id", BlockEntity.DISPENSER)
+                        .putInt("x", (int) this.x)
+                        .putInt("y", (int) this.y)
+                        .putInt("z", (int) this.z);
+                container = new BlockEntityDispenser(this.getLevel().getChunk((int) (this.x) >> 4, (int) (this.z) >> 4), nbt);
+            }
+
+            player.addWindow(container.getInventory());
+        }
+
+        return true;
+    }*/
 }
