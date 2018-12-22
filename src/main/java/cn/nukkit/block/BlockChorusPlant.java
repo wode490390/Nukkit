bc14f6275dcded6b4bcbae054e5d334020c7b0f6
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-22 20:27:19 +0800

    TODO: side check

--- src/main/java/cn/nukkit/block/BlockChorusPlant.java
+++ src/main/java/cn/nukkit/block/BlockChorusPlant.java
@@ -1,7 +1,10 @@
 package cn.nukkit.block;
 
+import cn.nukkit.Player;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemTool;
+import cn.nukkit.level.Level;
+import cn.nukkit.math.BlockFace;
 
 public class BlockChorusPlant extends BlockTransparent {
 
@@ -36,5 +39,27 @@ public class BlockChorusPlant extends BlockTransparent {
     @Override
     public Item[] getDrops(Item item) {
         return new Item[0];
+    }
+
+    @Override
+    public boolean place(Item item, Block block, Block target, BlockFace face, double fx, double fy, double fz, Player player) {
+        Block below = this.down();
+        if (below instanceof BlockEndStone || below instanceof BlockChorusPlant) {
+            this.getLevel().setBlock(block, this, true, true);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int onUpdate(int type) {
+        if (type == Level.BLOCK_UPDATE_NORMAL) {
+            Block below = this.down();
+            if (!(below instanceof BlockEndStone || below instanceof BlockChorusPlant)) {
+                this.getLevel().useBreakOn(this);
+                return Level.BLOCK_UPDATE_NORMAL;
+            }
+        }
+        return 0;
     }
 }
