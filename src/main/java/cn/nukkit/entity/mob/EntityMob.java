e43ccc1b6d265acb1624a6560abcd5705b25e060
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-27 01:16:11 +0800

    Add base hostile inventory

--- src/main/java/cn/nukkit/entity/mob/EntityMob.java
+++ src/main/java/cn/nukkit/entity/mob/EntityMob.java
@@ -12,6 +12,14 @@ import cn.nukkit.nbt.tag.CompoundTag;
  */
 public abstract class EntityMob extends EntityCreature {
 
+    protected Item hand = Item.get(Item.AIR);
+    protected Item offhand = Item.get(Item.AIR);
+
+    protected Item helmet = Item.get(Item.AIR);
+    protected Item chestplate = Item.get(Item.AIR);
+    protected Item leggings = Item.get(Item.AIR);
+    protected Item boots = Item.get(Item.AIR);
+
     public EntityMob(FullChunk chunk, CompoundTag nbt) {
         super(chunk, nbt);
     }
@@ -56,5 +64,85 @@ public abstract class EntityMob extends EntityCreature {
 
     public int getXpDropAmount() {
         return 0;
+    }
+
+    public Item getItemInHand() {
+        return this.hand;
+    }
+
+    public void setItemInHand(Item item) {
+        this.hand = item;
+    }
+
+    public Item getItemInOffhand() {
+        return this.offhand;
+    }
+
+    public void setItemInOffhand(Item item) {
+        this.offhand = item;
+    }
+
+    public Item getHelmet() {
+        return this.helmet;
+    }
+
+    public void setHelmet(Item item) {
+        if (item.isHelmet()) {
+            this.helmet = item;
+        } else {
+            this.helmet = Item.get(Item.AIR);
+        }
+    }
+
+    public Item getChestplate() {
+        return this.chestplate;
+    }
+
+    public void setChestplate(Item item) {
+        if (item.isChestplate()) {
+            this.chestplate = item;
+        } else {
+            this.chestplate = Item.get(Item.AIR);
+        }
+    }
+
+    public Item getLeggings() {
+        return this.leggings;
+    }
+
+    public void setLeggings(Item item) {
+        if (item.isLeggings()) {
+            this.leggings = item;
+        } else {
+            this.leggings = Item.get(Item.AIR);
+        }
+    }
+
+    public Item getBoots() {
+        return this.boots;
+    }
+
+    public void setBoots(Item item) {
+        if (item.isBoots()) {
+            this.boots = item;
+        } else {
+            this.boots = Item.get(Item.AIR);
+        }
+    }
+
+    public Item[] getArmorContents() {
+        return new Item[]{
+                this.getHelmet(),
+                this.getChestplate(),
+                this.getLeggings(),
+                this.getBoots()
+        };
+    }
+
+    public void clearAllArmors() {
+        this.setHelmet(Item.get(Item.AIR));
+        this.setChestplate(Item.get(Item.AIR));
+        this.setLeggings(Item.get(Item.AIR));
+        this.setBoots(Item.get(Item.AIR));
     }
 }
