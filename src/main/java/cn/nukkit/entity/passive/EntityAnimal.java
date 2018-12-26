7bd5e9c6f3fe6b8601c06d860082a319568fdd06
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-26 20:38:17 +0800

    baby

--- src/main/java/cn/nukkit/entity/passive/EntityAnimal.java
+++ src/main/java/cn/nukkit/entity/passive/EntityAnimal.java
@@ -12,14 +12,25 @@ import cn.nukkit.nbt.tag.CompoundTag;
  * author: MagicDroidX
  * Nukkit Project
  */
-public abstract class EntityAnimal extends EntityCreature implements EntityAgeable {
+public abstract class EntityAnimal extends EntityCreature {
+
     public EntityAnimal(FullChunk chunk, CompoundTag nbt) {
         super(chunk, nbt);
     }
 
-    @Override
     public boolean isBaby() {
-        return this.getDataFlag(DATA_FLAGS, Entity.DATA_FLAG_BABY);
+        return this.hasBaby() && this.getDataFlag(DATA_FLAGS, Entity.DATA_FLAG_BABY);
+    }
+
+    public void setBaby(boolean baby) {
+        if (this.hasBaby()) {
+            this.setDataFlag(DATA_FLAGS, DATA_FLAG_BABY, baby);
+            this.setScale(baby ? 0.5f : 1);
+        }
+    }
+
+    public boolean hasBaby() {
+        return false;
     }
 
     public boolean isBreedingItem(Item item) {
