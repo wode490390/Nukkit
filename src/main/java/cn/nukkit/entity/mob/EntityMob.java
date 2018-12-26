d78f7cd3e40e830f73bbfd0a1f850ebe52c521d2
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-26 20:26:11 +0800

    baby

--- src/main/java/cn/nukkit/entity/mob/EntityMob.java
+++ src/main/java/cn/nukkit/entity/mob/EntityMob.java
@@ -29,6 +29,22 @@ public abstract class EntityMob extends EntityCreature {
         return false;
     }
 
+    public void setBaby(boolean baby) {
+        if (this.hasBaby()) {
+            this.setDataFlag(DATA_FLAGS, DATA_FLAG_BABY, baby);
+            this.setScale(baby ? 0.5f : 1);
+            this.setMovementSpeed(baby ? 0.12f : 0.1f);
+        }
+    }
+
+    public boolean isBaby() {
+        return this.hasBaby() && this.getDataFlag(DATA_FLAGS, Entity.DATA_FLAG_BABY);
+    }
+
+    public boolean hasBaby() {
+        return false;
+    }
+
     public int getXpDropAmount() {
         return 0;
     }
