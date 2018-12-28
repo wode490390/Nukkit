d2e16bbdb360c6d59ddb959870a8c71b00e743e7
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-28 16:15:41 +0800

    Fix meta

---
+++ src/main/java/cn/nukkit/block/BlockFlowable.java
@@ -0,0 +1,36 @@
+package cn.nukkit.block;
+
+import cn.nukkit.math.AxisAlignedBB;
+
+public abstract class BlockFlowable extends BlockTransparent {
+
+    @Override
+    public boolean canBeFlowedInto() {
+        return true;
+    }
+
+    @Override
+    public boolean canPassThrough() {
+        return true;
+    }
+
+    @Override
+    public double getHardness() {
+        return 0;
+    }
+
+    @Override
+    public double getResistance() {
+        return 0;
+    }
+
+    @Override
+    public boolean isSolid() {
+        return false;
+    }
+
+    @Override
+    protected AxisAlignedBB recalculateBoundingBox() {
+        return null;
+    }
+}
