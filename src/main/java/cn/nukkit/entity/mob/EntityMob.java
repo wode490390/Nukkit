f2abdfb71191673bae5bc88cf65e51f80949e968
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-29 00:50:12 +0800

    oops

--- src/main/java/cn/nukkit/entity/mob/EntityMob.java
+++ src/main/java/cn/nukkit/entity/mob/EntityMob.java
@@ -42,7 +42,7 @@ public abstract class EntityMob extends EntityCreature {
         super.initEntity();
 
         if (this.hasBaby()) {
-            this.setDataFlag(DATA_FLAGS, DATA_FLAG_BABY, false)
+            this.setDataFlag(DATA_FLAGS, DATA_FLAG_BABY, false);
         }
     }
 
@@ -55,7 +55,7 @@ public abstract class EntityMob extends EntityCreature {
     }
 
     public boolean isBaby() {
-        return this.hasBaby() && this.getDataFlag(DATA_FLAGS, Entity.DATA_FLAG_BABY);
+        return this.hasBaby() && this.getDataFlag(DATA_FLAGS, DATA_FLAG_BABY);
     }
 
     public boolean hasBaby() {
