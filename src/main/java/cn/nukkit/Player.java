b05b0b4faf6c35324a206bf07554c3be61b801dc
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-28 15:51:51 +0800

    opti

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -3316,12 +3316,14 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
     }
 
     public void setViewDistance(int distance) {
-        this.chunkRadius = distance;
+        if (this.chunkRadius != distance) {
+            this.chunkRadius = distance;
 
-        ChunkRadiusUpdatedPacket pk = new ChunkRadiusUpdatedPacket();
-        pk.radius = distance;
+            ChunkRadiusUpdatedPacket pk = new ChunkRadiusUpdatedPacket();
+            pk.radius = distance;
 
-        this.dataPacket(pk);
+            this.dataPacket(pk);
+        }
     }
 
     public int getViewDistance() {
