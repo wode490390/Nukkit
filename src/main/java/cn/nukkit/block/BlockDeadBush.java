5c6bf15c0e4683f3224f9bac521ad34a3b09d5ba
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-30 02:36:33 +0800

    Fix place

--- src/main/java/cn/nukkit/block/BlockDeadBush.java
+++ src/main/java/cn/nukkit/block/BlockDeadBush.java
@@ -37,7 +37,7 @@ public class BlockDeadBush extends BlockFlowable {
     @Override
     public boolean place(Item item, Block block, Block target, BlockFace face, double fx, double fy, double fz, Player player) {
         Block down = this.down();
-        if (down.getId() == SAND || down.getId() == TERRACOTTA || down.getId() == STAINED_TERRACOTTA || down.getId() == PODZOL) {
+        if (down.getId() == SAND || down.getId() == TERRACOTTA || down.getId() == STAINED_TERRACOTTA || down.getId() == PODZOL || down.getId() == DIRT) {
             this.getLevel().setBlock(block, this, true, true);
             return true;
         }
