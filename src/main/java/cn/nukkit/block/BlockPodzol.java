e6819fb4134660ead1be09604251bac056f5a0d3
Wode <17339354+wode490390@users.noreply.github.com>
2018-12-30 02:39:05 +0800

    Fix meta

--- src/main/java/cn/nukkit/block/BlockPodzol.java
+++ src/main/java/cn/nukkit/block/BlockPodzol.java
@@ -4,9 +4,10 @@ package cn.nukkit.block;
  * Created on 2015/11/22 by xtypr.
  * Package cn.nukkit.block in project Nukkit .
  */
-public class BlockPodzol extends BlockDirt {
+public class BlockPodzol extends BlockSolid {
 
     public BlockPodzol() {
+
     }
 
     @Override
@@ -20,7 +21,34 @@ public class BlockPodzol extends BlockDirt {
     }
 
     @Override
+    public double getResistance() {
+        return 2.5;
+    }
+
+    @Override
+    public double getHardness() {
+        return 0.5;
+    }
+
+    @Override
+    public int getToolType() {
+        return ItemTool.TYPE_SHOVEL;
+    }
+
+    @Override
     public boolean canSilkTouch() {
         return true;
+    }
+
+    @Override
+    public Item[] getDrops(Item item) {
+        return new Item[]{
+                new ItemBlock(new BlockDirt())
+        };
+    }
+
+    @Override
+    public BlockColor getColor() {
+        return BlockColor.DIRT_BLOCK_COLOR;
     }
 }
