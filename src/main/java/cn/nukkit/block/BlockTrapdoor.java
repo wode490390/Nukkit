fb9dd5e1b5c74639193dc636f5fd95d2765acf03
Wode <17339354+wode490390@users.noreply.github.com>
2019-01-03 11:37:26 +0800

    Fix face

--- src/main/java/cn/nukkit/block/BlockTrapdoor.java
+++ src/main/java/cn/nukkit/block/BlockTrapdoor.java
@@ -219,8 +219,17 @@ public class BlockTrapdoor extends BlockTransparentMeta {
     }
 
     public BlockFace getFacing() {
-        int[] faces = {3, 1, 0, 2};
-        return BlockFace.fromHorizontalIndex(faces[this.getDamage() & 0x03]);
+        switch (meta & 0x03) {
+            case 0:
+                return BlockFace.NORTH;
+            case 1:
+                return BlockFace.SOUTH;
+            case 2:
+                return BlockFace.WEST;
+            case 3:
+            default:
+                return BlockFace.EAST;
+        }
     }
 
     public boolean isOpen() {
