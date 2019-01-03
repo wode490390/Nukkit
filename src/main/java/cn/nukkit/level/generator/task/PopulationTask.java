898af567b8fed9c7755e5ee6275353ebc146956b
Wode <17339354+wode490390@users.noreply.github.com>
2019-01-03 08:08:08 +0800

    light

--- src/main/java/cn/nukkit/level/generator/task/PopulationTask.java
+++ src/main/java/cn/nukkit/level/generator/task/PopulationTask.java
@@ -17,7 +17,7 @@ public class PopulationTask extends AsyncTask {
     private boolean state;
     private BaseFullChunk centerChunk;
 
-    public final BaseFullChunk[] chunks = new BaseFullChunk[9];
+    public final BaseFullChunk[] chunks;
 
     public PopulationTask(Level level, BaseFullChunk chunk) {
         this.state = true;
@@ -25,16 +25,7 @@ public class PopulationTask extends AsyncTask {
         this.centerChunk = chunk;
         this.seed = level.getSeed();
 
-        chunks[4] = chunk;
-
-        int i = 0;
-        for (int z = -1; z <= 1; z++) {
-            for (int x = -1; x <= 1; x++, i++) {
-                if (i == 4) continue;
-                BaseFullChunk ck = level.getChunk(chunk.getX() + x, chunk.getZ() + z, true);
-                this.chunks[i] = ck;
-            }
-        }
+        this.chunks = level.getAdjacentChunks(chunk.getX(), chunk.getZ());
     }
 
 
@@ -112,7 +103,7 @@ public class PopulationTask extends AsyncTask {
                     centerChunk = manager.getChunk(centerChunk.getX(), centerChunk.getZ());
                     centerChunk.setPopulated();
                     centerChunk.recalculateHeightMap();
-                    centerChunk.populateSkyLight();
+                    centerChunk.populateSkyLight(manager);
                     centerChunk.setLightPopulated();
                     this.centerChunk = centerChunk;
                 }
