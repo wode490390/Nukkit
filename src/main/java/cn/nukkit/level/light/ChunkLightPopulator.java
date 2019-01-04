70580599ecbae91571bc34dd51cef56426247878
Wode <17339354+wode490390@users.noreply.github.com>
2019-01-04 08:51:15 +0800

    revert

--- src/main/java/cn/nukkit/level/light/ChunkLightPopulator.java
+++ src/main/java/cn/nukkit/level/light/ChunkLightPopulator.java
@@ -2,7 +2,7 @@ package cn.nukkit.level.light;
 
 import cn.nukkit.Server;
 import cn.nukkit.level.Level;
-import cn.nukkit.level.format.anvil.Chunk;
+import cn.nukkit.level.format.generic.BaseFullChunk;
 import cn.nukkit.scheduler.AsyncTask;
 
 /**
@@ -10,29 +10,40 @@ import cn.nukkit.scheduler.AsyncTask;
  */
 public class ChunkLightPopulator extends AsyncTask {
 
-    protected Level level;
+    private final Level level;
+    protected BaseFullChunk chunk;
 
-    protected Chunk chunk;
+    protected LightUpdate blockLightUpdates;
+    protected LightUpdate skyLightUpdates;
 
-    protected LightUpdate blockLightUpdates = null;
-    protected LightUpdate skyLightUpdates = null;
-
-    public ChunkLightPopulator(Level level, Chunk chunk) {
+    public ChunkLightPopulator(Level level, BaseFullChunk chunk) {
         this.level = level;
-        this.chunk = (Chunk) chunk.clone();
+        this.chunk = chunk;
     }
 
     @Override
     public void onRun() {
+        BaseFullChunk chunk = this.chunk.clone();
+        if (chunk == null) {
+            return;
+        }
+
         chunk.recalculateHeightMap();
         chunk.populateSkyLight();
         chunk.setLightPopulated();
+
+        this.chunk = chunk.clone();
     }
 
     @Override
     public void onCompletion(Server server) {
-        if (!level.isClosed()) {
-            level.generateChunkCallback(chunk.getX(), chunk.getZ(), chunk);
+        BaseFullChunk chunk = this.chunk.clone();
+        if (this.level != null) {
+            if (chunk == null) {
+                return;
+            }
+
+            this.level.generateChunkCallback(chunk.getX(), chunk.getZ(), chunk);
         }
     }
 }
