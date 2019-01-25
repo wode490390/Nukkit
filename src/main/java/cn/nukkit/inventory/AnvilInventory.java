b3f4d779dc2d53ca30988d438197b1f30613dab8
Wode <17339354+wode490390@users.noreply.github.com>
2019-01-25 09:04:23 +0800

    b810716

--- src/main/java/cn/nukkit/inventory/AnvilInventory.java
+++ src/main/java/cn/nukkit/inventory/AnvilInventory.java
@@ -4,8 +4,7 @@ import cn.nukkit.Player;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.enchantment.Enchantment;
 import cn.nukkit.level.Position;
-import cn.nukkit.level.Sound;
-
+import cn.nukkit.network.protocol.LevelSoundEventPacket;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -51,7 +50,7 @@ public class AnvilInventory extends ContainerInventory {
             player.getInventory().sendContents(player);
             sendContents(player);
 
-            player.getLevel().addSound(player, Sound.RANDOM_ANVIL_USE);
+            player.getLevel().addLevelSoundEvent(player, LevelSoundEventPacket.SOUND_RANDOM_ANVIL_USE);
             return true;
         } else if (local.getId() != 0 && second.getId() != 0) { //enchants combining
             if (!local.equals(second, true, false)) {
@@ -118,7 +117,7 @@ public class AnvilInventory extends ContainerInventory {
                 clearAll();
                 sendContents(player);
 
-                player.getLevel().addSound(player, Sound.RANDOM_ANVIL_USE);
+                player.getLevel().addLevelSoundEvent(player, LevelSoundEventPacket.SOUND_RANDOM_ANVIL_USE);
                 return true;
             }
         }
