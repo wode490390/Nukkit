5e7dbe4c28c39969594f5483e955c91cea5c4b7e
Wode <17339354+wode490390@users.noreply.github.com>
2019-01-31 21:47:22 +0800

    Add player request resource packs event

---
+++ src/main/java/cn/nukkit/event/player/PlayerRequestResourcePackEvent.java
@@ -0,0 +1,56 @@
+package cn.nukkit.event.player;
+
+import cn.nukkit.Player;
+import cn.nukkit.event.HandlerList;
+import cn.nukkit.resourcepacks.ResourcePack;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlayerRequestResourcePackEvent extends PlayerEvent {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    public static HandlerList getHandlers() {
+        return handlers;
+    }
+
+    private final Map<String, ResourcePack> resourcePacks = new HashMap<>();
+    private final Map<String, ResourcePack> behaviourPacks = new HashMap<>();
+    private boolean mustAccept;
+
+    public PlayerRequestResourcePackEvent(Player player, Map<String, ResourcePack> resourcePacks, Map<String, ResourcePack> behaviourPacks, boolean mustAccept) {
+        this.player = player;
+        this.resourcePacks.putAll(resourcePacks);
+        this.behaviourPacks.putAll(behaviourPacks);
+        this.mustAccept = mustAccept;
+    }
+
+    public Map<String, ResourcePack> getResourcePacks() {
+        return resourcePacks;
+    }
+
+    public void putResourcePack(ResourcePack... pack) {
+        for (ResourcePack p : pack) {
+            this.resourcePacks.put(p.getPackId(), p);
+        }
+    }
+
+    public Map<String, ResourcePack> getBehaviourPacks() {
+        return behaviourPacks;
+    }
+
+    public void putBehaviourPack(ResourcePack... pack) {
+        for (ResourcePack p : pack) {
+            this.behaviourPacks.put(p.getPackId(), p);
+        }
+    }
+
+    public boolean isMustAccept() {
+        return mustAccept;
+    }
+
+    public PlayerRequestResourcePackEvent setMustAccept(boolean mustAccept) {
+        this.mustAccept = mustAccept;
+        return this;
+    }
+}
