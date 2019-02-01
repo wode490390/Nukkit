36f074c3a99fbfe9cad2f34c198d170281067085
Wode <17339354+wode490390@users.noreply.github.com>
2019-02-01 18:08:29 +0800

    Fix drop exp

--- src/main/java/cn/nukkit/block/BlockOreQuartz.java
+++ src/main/java/cn/nukkit/block/BlockOreQuartz.java
@@ -4,8 +4,6 @@ import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemQuartz;
 import cn.nukkit.item.ItemTool;
 import cn.nukkit.item.enchantment.Enchantment;
-import cn.nukkit.math.NukkitRandom;
-
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -15,6 +13,7 @@ import java.util.concurrent.ThreadLocalRandom;
 public class BlockOreQuartz extends BlockSolid {
 
     public BlockOreQuartz() {
+
     }
 
     @Override
@@ -67,7 +66,7 @@ public class BlockOreQuartz extends BlockSolid {
 
     @Override
     public int getDropExp() {
-        return new NukkitRandom().nextRange(1, 5);
+        return ThreadLocalRandom.current().nextInt(2, 5);
     }
 
     @Override
