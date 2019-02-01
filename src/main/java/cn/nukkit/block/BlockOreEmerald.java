66960418235dd83fc492116a32b37d98dfe3d731
Wode <17339354+wode490390@users.noreply.github.com>
2019-02-01 17:51:03 +0800

    Fix resistance

--- src/main/java/cn/nukkit/block/BlockOreEmerald.java
+++ src/main/java/cn/nukkit/block/BlockOreEmerald.java
@@ -4,8 +4,6 @@ import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemEmerald;
 import cn.nukkit.item.ItemTool;
 import cn.nukkit.item.enchantment.Enchantment;
-import cn.nukkit.math.NukkitRandom;
-
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -15,6 +13,7 @@ import java.util.concurrent.ThreadLocalRandom;
 public class BlockOreEmerald extends BlockSolid {
 
     public BlockOreEmerald() {
+
     }
 
     @Override
@@ -39,7 +38,7 @@ public class BlockOreEmerald extends BlockSolid {
 
     @Override
     public double getResistance() {
-        return 15;
+        return 5;
     }
 
     @Override
@@ -67,7 +66,7 @@ public class BlockOreEmerald extends BlockSolid {
 
     @Override
     public int getDropExp() {
-        return new NukkitRandom().nextRange(3, 7);
+        return ThreadLocalRandom.current().nextInt(3, 7);
     }
 
     @Override
