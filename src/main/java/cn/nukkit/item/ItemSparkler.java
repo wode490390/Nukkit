97da8f72cac1828b7ed2119bea96f9b1d8f886fe
Wode <17339354+wode490390@users.noreply.github.com>
2019-02-02 11:56:45 +0800

    extends ItemTool

--- src/main/java/cn/nukkit/item/ItemSparkler.java
+++ src/main/java/cn/nukkit/item/ItemSparkler.java
@@ -1,6 +1,6 @@
 package cn.nukkit.item;
 
-public class ItemSparkler extends Item {
+public class ItemSparkler extends ItemTool {
 
     public ItemSparkler() {
         this(0, 1);
@@ -13,6 +13,11 @@ public class ItemSparkler extends Item {
     public ItemSparkler(Integer meta, int count) {
         super(SPARKLER, meta, count, "Sparkler");
     }
+
+    //@Override
+    //public int getMaxDurability() {
+    //    return ItemTool.DURABILITY_SPARKLER; //TODO: check
+    //}
 
     @Override
     public boolean isEducation() {
