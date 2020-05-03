6f274f187ad45481c1d8d03d55b7089e21b82080
Wodor <17339354+wode490390@users.noreply.github.com>
2020-05-03 14:09:21 +0800

    Revert "Fix NBT loading errors (hotfix) (#1391)"
    This reverts commit c43d42528eaaa9ca3474eb54fd6870e32fea2647.

--- src/main/java/cn/nukkit/level/GlobalBlockPalette.java
+++ src/main/java/cn/nukkit/level/GlobalBlockPalette.java
@@ -4,11 +4,9 @@ import cn.nukkit.Server;
 import cn.nukkit.nbt.NBTIO;
 import cn.nukkit.nbt.tag.CompoundTag;
 import cn.nukkit.nbt.tag.ListTag;
-import com.google.common.io.ByteStreams;
 import it.unimi.dsi.fastutil.ints.Int2IntMap;
 import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteOrder;
@@ -26,13 +24,14 @@ public class GlobalBlockPalette {
         legacyToRuntimeId.defaultReturnValue(-1);
         runtimeIdToLegacy.defaultReturnValue(-1);
 
+        InputStream stream = Server.class.getClassLoader().getResourceAsStream("runtime_block_states.dat");
+        if (stream == null) {
+            throw new AssertionError("Unable to locate block state nbt");
+        }
         ListTag<CompoundTag> tag;
-        try (InputStream stream = Server.class.getClassLoader().getResourceAsStream("runtime_block_states.dat")) {
-            if (stream == null) {
-                throw new AssertionError("Unable to locate block state nbt");
-            }
+        try {
             //noinspection unchecked
-            tag = (ListTag<CompoundTag>) NBTIO.readTag(new ByteArrayInputStream(ByteStreams.toByteArray(stream)), ByteOrder.LITTLE_ENDIAN, false);
+            tag = (ListTag<CompoundTag>) NBTIO.readTag(stream, ByteOrder.LITTLE_ENDIAN, false);
         } catch (IOException e) {
             throw new AssertionError("Unable to load block palette", e);
         }
