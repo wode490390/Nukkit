d00a8a8230b01d336d9ccded0801f0941568eaad
Wodor <17339354+wode490390@users.noreply.github.com>
2020-06-27 12:29:03 +0800

    Revert "NPE fix for `EntityHuman` NPCs that are already saved (#1353)"
    This reverts commit e69ac841a7e3a6c8d91ae2b89b710c86c1998db5.

--- src/main/java/cn/nukkit/entity/data/Skin.java
+++ src/main/java/cn/nukkit/entity/data/Skin.java
@@ -47,7 +47,7 @@ public class Skin {
     private String capeId;
     private String skinColor = "#0";
     private String armSize = "wide";
-    private boolean trusted = false;
+    private boolean trusted;
 
     public boolean isValid() {
         return isValidSkin() && isValidResourcePatch();
